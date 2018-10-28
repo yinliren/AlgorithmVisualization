9b9a7a10737651a9b7107c187ba63ca2b3c55f64
Liren Yin <28661619+yinliren@users.noreply.github.com>
2018-10-28 13:51:19 -0700

    Add files via upload

---
+++ src/RandomMoneyGiving/Main.java
@@ -0,0 +1,12 @@
+package RandomMoneyGiving;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sceneWidth = 1000;
+        int sceneHeight = 400;
+
+        AlgoVisualizer vis = new AlgoVisualizer(sceneWidth, sceneHeight);
+    }
+}
